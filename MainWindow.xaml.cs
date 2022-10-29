0885abf79996f7ca5dd711a241da9b82ce05251c
Anton Slastuhin <antonslastuhin@gmail.com>
2022-10-29 19:46:42 +0300

    тестирую docx

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,6 @@
-﻿using GemBox.Document;
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,6 +14,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using WomenConsulting.Interfaces;
+using Xceed.Words.NET;
 
 namespace WomenConsulting
 {
@@ -31,13 +32,21 @@ namespace WomenConsulting
 
         private void InitPages()
         {
-            //инициализация бесплатной версии GemBox
-            ComponentInfo.SetLicense("FREE-LIMITED-KEY");
-
+            Test();
             trimestrs = new List<ITrimestr>()
             {
 
             };
+        }
+
+        private void Test()
+        {
+            var path = "MalenkiySrokFinal.docx";
+
+            using (var fs = new FileStream(path,FileMode.Open))
+            {
+                var doc = DocX.Load(fs);
+            }
         }
     }
 }

7ef650c5a3a2c9ff47b21f1f9c1d4994597e4633
Евгений Яшин <y4shin1@yandex.ru>
2022-11-20 17:45:04 +0300

    Класс Настроек

---
+++ Class/Settings.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace WomenConsulting.Class
+{
+    [Serializable]
+    class Settings
+    {
+        public List<string> Doctors { get; set; }
+        public string LastOpenDirectory { get; set; }
+        public Settings()
+        {
+
+        }
+        public void AddDoctor(string NewDoctor)
+        {
+            Doctors.Add(NewDoctor);
+        }
+
+        //убрать по индексу
+        //public void RemoveDoctor(int Position)
+        //{
+        //    Doctors.RemoveAt(Position);
+        //}
+        
+        //убрать по имени
+        public void RemoveDoctor(string RemovedDoctor)
+        {
+            Doctors.Remove(RemovedDoctor);
+        }
+
+        public List<string> GetDoctors()
+        {
+            return Doctors;
+        }
+
+        public void UpdateLastOpened(string NewPath)
+        {
+            LastOpenDirectory = NewPath;
+        }
+
+        public string GetLastOpened()
+        {
+            return LastOpenDirectory;
+        }
+    }
+}

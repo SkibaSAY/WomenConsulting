4ad7b6b4cbab67248e43a6bc93bc6661bda3400e
pr0gramm-ist <pr0gramm-ist@yandex.ru>
2023-02-05 15:37:44 +0300

    Создали класс для малых сроков

---
+++ Class/MillimetrWeekTable.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WomenConsulting
+{
+    public class WeekAndDay
+    {
+        public int weeks;
+        public int days;
+
+        public WeekAndDay(int weeks = 0, int days = 0)
+        {
+            this.weeks = weeks;
+            this.days = days;
+        }
+    }
+
+    public class MillimetrWeekTable
+    {
+        public Dictionary<int, WeekAndDay> MatkaTable;
+        public Dictionary<int, WeekAndDay> KTRTable;
+        public Dictionary<int, WeekAndDay> EggTable;
+
+        public MillimetrWeekTable()
+        {
+            Init();
+        }
+
+        public void Init()
+        {
+
+        }
+    }
+}
